1ef7f5c172d0884e56b6a28711ee0899fb9ba575
Dana Allen <dana.allen@shaw.ca>
2018-11-11 15:15:06 -0800

    full signup done

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivity.java
@@ -1,30 +1,48 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import cmput301f18t18.health_detective.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button signUp = findViewById(R.id.signUpBtn);
+        signUp.setOnClickListener(this);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu){
-//        getMenuInflater().inflate(R.menu.signup_menu, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.signup_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,MainActivity.class);
+        changeActivity(intent);
+    }
+
+    public void changeActivity(Intent intent){
+        startActivity(intent);
+    }
 }
