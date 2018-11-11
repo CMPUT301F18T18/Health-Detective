70dea528dd9a9c18f8cae96de82686dc61af9e62
Dana Allen <dana.allen@shaw.ca>
2018-11-11 13:26:51 -0800

    signup creation

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivity.java
@@ -2,6 +2,10 @@ package cmput301f18t18.health_detective.presentation.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 import cmput301f18t18.health_detective.R;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -11,4 +15,16 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.signup_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        return true;
+//    }
+
 }
