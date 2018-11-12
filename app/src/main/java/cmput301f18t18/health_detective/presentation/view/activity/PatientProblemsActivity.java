7c66f77cfa65b6e3008aadb364c2a3c1a67cc695
Dana Allen <dana.allen@shaw.ca>
2018-11-12 13:10:14 -0800

    recycler view for problem

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientProblemsActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientProblemsActivity.java
@@ -3,6 +3,7 @@ package cmput301f18t18.health_detective.presentation.view.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,10 +11,13 @@ import cmput301f18t18.health_detective.R;
 
 public class PatientProblemsActivity extends AppCompatActivity implements View.OnClickListener{
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_problems);
+
+
 
         Button addProblem = (Button)findViewById(R.id.addProbBtn);
         addProblem.setOnClickListener(this);
