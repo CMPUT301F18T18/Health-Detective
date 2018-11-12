8fb2fa97aea43c1d99fe852b008520c5183eccb0
Dana Allen <dana.allen@shaw.ca>
2018-11-12 13:44:22 -0800

    delete pencil

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/MainActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/MainActivity.java
@@ -1,13 +1,11 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import cmput301f18t18.health_detective.R;
 
