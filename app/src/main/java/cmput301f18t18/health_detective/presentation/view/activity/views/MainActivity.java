a0065d5df05021063b555370e18d5c53faf37747
Jordan McCarthy <jordanmccarthy@Jordans-MacBook-Pro.local>
2018-11-12 14:41:43 -0700

    added subfolders

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/views/MainActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/views/MainActivity.java
@@ -1,13 +1,11 @@
-package cmput301f18t18.health_detective.presentation.view.activity;
+package cmput301f18t18.health_detective.presentation.view.activity.views;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import cmput301f18t18.health_detective.R;
 
--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/views/PatientRecordViewActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/views/PatientRecordViewActivity.java
@@ -1,15 +1,15 @@
-package cmput301f18t18.health_detective.presentation.view.activity;
+package cmput301f18t18.health_detective.presentation.view.activity.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cmput301f18t18.health_detective.R;
 
-public class PatientRecordsActivity extends AppCompatActivity {
+public class PatientRecordViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_records);
+        setContentView(R.layout.activity_patient_record_view);
     }
 }
