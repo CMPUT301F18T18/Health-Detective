b91fab99f0f688b968bd44dd8357211c36f7b08d
Dana Allen <dana.allen@shaw.ca>
2018-11-12 21:08:48 -0800

    record list addition

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientProblemsActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientProblemsActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -33,8 +37,6 @@ public class PatientProblemsActivity extends AppCompatActivity implements View.O
         //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         //setSupportActionBar(myToolbar);
 
-
-
         Button addProblem = (Button)findViewById(R.id.addProbBtn);
         addProblem.setOnClickListener(this);
 
@@ -45,8 +47,41 @@ public class PatientProblemsActivity extends AppCompatActivity implements View.O
 
         adapter = new ProblemListAdapter(this, testList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent problemsIntent = new Intent(PatientProblemsActivity.this, PatientRecordsActivity.class);
+                changeActivity(problemsIntent);
+
+            }
+        });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // being able to use the menu at the top of the app
+        getMenuInflater().inflate(R.menu.problem_list_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.app_bar_search:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordsActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordsActivity.java
@@ -1,15 +1,34 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import cmput301f18t18.health_detective.R;
 
 public class PatientRecordsActivity extends AppCompatActivity {
 
+    ListView listView;
+    RecordListAdapter adapter;
+    ArrayList<String> testList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_records);
+
+        listView = findViewById(R.id.recordListView);
+        testList.add("test");
+        testList.add("test2");
+
+        adapter = new RecordListAdapter(this, testList);
+        listView.setAdapter(adapter);
+
     }
+
 }
