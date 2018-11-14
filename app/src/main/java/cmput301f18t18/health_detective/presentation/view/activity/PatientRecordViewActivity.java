1114a916c9ba6c7504c09d53680889552bda4c21
Dana Allen <dana.allen@shaw.ca>
2018-11-13 19:55:36 -0800

    adding record view

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordViewActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordViewActivity.java
@@ -1,12 +1,15 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import cmput301f18t18.health_detective.R;
 
@@ -20,16 +23,31 @@ public class PatientRecordViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_patient_record_view);
 
         //stuff for all photos section
-
         GridViewAdapter adapter = new GridViewAdapter(this, 4);
         GridView gridView = (GridView) findViewById(R.id.allPhotosView);
+
         gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(PatientRecordViewActivity.this, "Photo Click", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
 
-        // Stuff for body location photos
+        // Stuff for body location photos section
         bodyPhotoScroll = (LinearLayout) findViewById(R.id.root);
         ImageView testImg = new ImageView(this);
         testImg.setImageResource(R.drawable.ic_launcher_background);
+
+        bodyPhotoScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(PatientRecordViewActivity.this, "BL Click", Toast.LENGTH_SHORT);
+            toast.show();
+            }
+        });
         test();
         test();
         test();
