1c0dee8300dc29c84993e85a5acb03fb32a83c46
Dana Allen <dana.allen@shaw.ca>
2018-11-14 14:42:23 -0800

    working before search merge

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordViewActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/PatientRecordViewActivity.java
@@ -1,6 +1,12 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +16,17 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 import cmput301f18t18.health_detective.R;
 
@@ -60,6 +77,5 @@ public class PatientRecordViewActivity extends AppCompatActivity {
         bodyPhotoScroll.addView(testImg);
         bodyPhotoScroll.invalidate();
     }
-
 
 }
