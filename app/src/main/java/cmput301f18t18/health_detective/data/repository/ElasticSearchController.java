84ef0abe0f425691a12a99cb004cee5ef8ddf099
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2018-11-14 16:36:47 -0700

    started implementing get problem

--- app/src/main/java/cmput301f18t18/health_detective/data/repository/ElasticSearchController.java
+++ app/src/main/java/cmput301f18t18/health_detective/data/repository/ElasticSearchController.java
@@ -1,11 +1,14 @@
 package cmput301f18t18.health_detective.data.repository;
 
+import android.util.Log;
+
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import cmput301f18t18.health_detective.domain.model.Problem;
 import cmput301f18t18.health_detective.domain.model.Record;
@@ -15,6 +18,9 @@ import cmput301f18t18.health_detective.domain.repository.RecordRepo;
 import cmput301f18t18.health_detective.domain.repository.UserRepo;
 import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.SearchResult.Hit;
 
 public class ElasticSearchController implements ProblemRepo, RecordRepo, UserRepo {
     private static final ElasticSearchController ourInstance = new ElasticSearchController();
@@ -62,6 +68,22 @@ public class ElasticSearchController implements ProblemRepo, RecordRepo, UserRep
 
     @Override
     public Problem retrieveProblemById(Integer problemID) {
+//        setClient();
+//        String query= "";
+//        Search search = new Search.Builder(query)
+//                .addIndex("cmput301f18t18")
+//                .addType("problem")
+//                .build();
+//        try {
+//            SearchResult elasticRes = client.execute(search);
+//            List<Hit<Problem, Void>> problems = elasticRes.getHits(Problem.class);
+//
+//            for (Hit<Problem, Void> hit : problems) {
+//                Problem prob = hit.source;
+//                Log.d("retrieveProblemByID", prob.getDescription());
+//            }
+//
+//        } catch (IOException e) { }
         return null;
     }
 
