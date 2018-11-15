54fb569549d336d7613685e7bda47304c593ba81
Dana Allen <dana.allen@shaw.ca>
2018-11-14 19:33:34 -0800

    search view

---
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/BodyPartCheckBoxAdapter.java
@@ -0,0 +1,129 @@
+package cmput301f18t18.health_detective.presentation.view.activity;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Set;
+
+import cmput301f18t18.health_detective.R;
+
+//https://stackoverflow.com/questions/38417984/android-spinner-dropdown-checkbox
+
+public class BodyPartCheckBoxAdapter extends BaseAdapter {
+
+    static class SpinnerItem<T> {
+        private String txt;
+        private T item;
+
+        SpinnerItem(T t, String s) {
+            item = t;
+            txt = s;
+        }
+    }
+
+    private Context context;
+    private Set<String> selected_items;
+    private List<SpinnerItem<String>> all_items;
+    private String headerText;
+
+    BodyPartCheckBoxAdapter(Context context,
+                    String headerText,
+                    List<SpinnerItem<String>> all_items,
+                    Set<String> selected_items) {
+        this.context = context;
+        this.headerText = headerText;
+        this.all_items = all_items;
+        this.selected_items = selected_items;
+    }
+
+    @Override
+    public int getCount() {
+        return all_items.size() + 1;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if( position < 1 ) {
+            return null;
+        }
+        else {
+            return all_items.get(position-1);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    @SuppressWarnings({"unchecked"})
+    @NonNull
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+        final ViewHolder holder;
+        if (convertView == null ) {
+            LayoutInflater layoutInflator = LayoutInflater.from(context);
+            convertView = layoutInflator.inflate(R.layout.drop_down_checkbox, parent, false);
+
+            holder = new ViewHolder();
+            holder.mTextView = convertView.findViewById(R.id.text);
+            holder.mCheckBox = convertView.findViewById(R.id.checkbox);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        if( position < 1 ) {
+            holder.mCheckBox.setVisibility(View.GONE);
+            holder.mTextView.setText(headerText);
+        }
+        else {
+            final int listPos = position - 1;
+            holder.mCheckBox.setVisibility(View.VISIBLE);
+            holder.mTextView.setText(all_items.get(listPos).txt);
+
+            final String item = all_items.get(listPos).item;
+            boolean isSel = selected_items.contains(item);
+
+            holder.mCheckBox.setOnCheckedChangeListener(null);
+            holder.mCheckBox.setChecked(isSel);
+
+            holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if( isChecked ) {
+                        selected_items.add(item);
+                    }
+                    else {
+                        selected_items.remove(item);
+                    }
+                }
+            });
+
+            holder.mTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    holder.mCheckBox.toggle();
+                }
+            });
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        private TextView mTextView;
+        private CheckBox mCheckBox;
+    }
+
+}
--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SearchActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/SearchActivity.java
@@ -3,14 +3,43 @@ package cmput301f18t18.health_detective.presentation.view.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import cmput301f18t18.health_detective.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private final List<BodyPartCheckBoxAdapter.SpinnerItem<String>> spinner_items = new ArrayList<>();
+    private final Set<String> selected_items = new HashSet<>();
+    List<String> bodyParts = new ArrayList<>(Arrays.asList("Left Leg", "Right Leg", "Left Arm", "Right Arm", "Head"));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // fill the 'spinner_items' array with all items to show
+        //List<String> all_objects = new ArrayList<>(Arrays.asList("Left Leg", "Right Leg", "Left Arm", "Right Arm", "Head"));
+        //all_objects.add("test");// from wherever
+        for(String o : bodyParts) {
+            spinner_items.add(new BodyPartCheckBoxAdapter.SpinnerItem<>(o, o));
+        }
+
+        // to start with any pre-selected, add them to the `selected_items` set
+
+        String headerText = "Body Part";
+
+        Spinner spinner = findViewById(R.id.spinner);
+        BodyPartCheckBoxAdapter adapter = new BodyPartCheckBoxAdapter(this, headerText, spinner_items, selected_items);
+        spinner.setAdapter(adapter);
+
+        // when you want to see what the user has selected, look in the `selected_items`
+        // set anywhere in your activity
     }
 }
