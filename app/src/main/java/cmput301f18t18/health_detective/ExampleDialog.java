59dfa37116c7848293614ce2098ea6eb78016eab
Jordan McCarthy <jordanmccarthy@Jordans-MacBook-Pro.local>
2018-11-18 22:34:58 -0700

    dialog added not implemented yet

---
+++ app/src/main/java/cmput301f18t18/health_detective/ExampleDialog.java
@@ -0,0 +1,41 @@
+package cmput301f18t18.health_detective;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import cmput301f18t18.health_detective.R;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+    private EditText editTextTitle;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog,null);
+
+        builder.setView(view)
+                .setTitle("Edit Title")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        editTextTitle = view.findViewById(R.id.edit_title_dialog);
+
+        return builder.create();
+    }
+}
