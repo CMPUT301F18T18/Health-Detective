776a0a4f24ec653074ff5ba08fd41afde992f85d
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2018-11-20 11:42:41 -0700

    Initial testing with sqlite

---
+++ app/src/main/java/cmput301f18t18/health_detective/data/repository/DbHelper.java
@@ -0,0 +1,13 @@
+package cmput301f18t18.health_detective.data.repository;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "test.db";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+}
