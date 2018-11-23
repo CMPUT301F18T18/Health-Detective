b966659cb96b8d660a8a4afc210adf3d12de1225
Jordan McCarthy <jordanmccarthy@Jordans-MacBook-Pro.local>
2018-11-22 18:48:38 -0700

    minor update

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/ProblemEditAddActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/ProblemEditAddActivity.java
@@ -115,6 +115,7 @@ public class ProblemEditAddActivity extends AppCompatActivity implements View.On
                     problemDateTime = new Date();
                 }
                 if (type){
+                    problemDateTime = problemContext.getStartDate();
                     problemAddEditPresenter.editUserProblem(problemContext, probTitle, probDesc, problemDateTime);
                 }
                 else {
