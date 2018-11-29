608d6551fd05f382837c67059ab148a834e49ae6
zhenhui zhou <zhenhui@ualberta.ca>
2018-11-28 23:32:09 -0700

    Draft map functions

--- app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/MapActivity.java
+++ app/src/main/java/cmput301f18t18/health_detective/presentation/view/activity/MapActivity.java
@@ -1,15 +1,36 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+
+import java.util.ConcurrentModificationException;
+import java.util.Locale;
+
 import cmput301f18t18.health_detective.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap myMap;
+    public static final float INITIAL_ZOOM = 12f;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,5 +83,62 @@ public class MapActivity extends AppCompatActivity {
 
     private void changeActivity(Intent intent) {
         startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        myMap = googleMap;
+
+        LatLng home = new LatLng(53.526973, -113.527114);
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, INITIAL_ZOOM));
+
+        setMapLongClick(myMap);
+        setPoiClick(myMap);
+
+    }
+
+    private void setMapLongClick(final GoogleMap map) {
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                String snippet = String.format(Locale.getDefault(),
+                        getString(R.string.lat_long_snippet),
+                        latLng.latitude,
+                        latLng.longitude);
+
+                myMap.addMarker(new MarkerOptions().
+                        position(latLng).
+                        title(getString(R.string.dropped_pin))
+                        .snippet(snippet)
+                        .icon(BitmapDescriptor.defaultMarker
+                                (BitmapDescriptorFactory.HUE_BLUE)));
+            }
+        });
+
+    }
+    private void setPoiClick(final GoogleMap map){
+        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
+            @Override
+            public void onPoiClick(PointOfInterest poi) {
+                Marker poiMarker = map.addMarker(new MarkerOptions()
+                        .position(poi.latLng)
+                        .title(poi.name));
+                poiMarker.showInfoWindow();
+                poiMarker.setTag(getString(R.string.poi));
+            }
+        });
+    }
+    private void enableMyLocation(GoogleMap map){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
     }
     }
