5b9094862cb47bcfde56cbe59c25d4c6877aaad2
Dalin Riches <dalinriches@gmail.com>
2018-11-28 19:43:41 -0700

    Fixed mistake in the add body location intractor requiring a body location

--- app/src/main/java/cmput301f18t18/health_detective/domain/interactors/impl/AddBodyLocationImpl.java
+++ app/src/main/java/cmput301f18t18/health_detective/domain/interactors/impl/AddBodyLocationImpl.java
@@ -3,20 +3,21 @@ package cmput301f18t18.health_detective.domain.interactors.impl;
 import cmput301f18t18.health_detective.domain.interactors.AddBodyLocation;
 import cmput301f18t18.health_detective.domain.interactors.base.AbstractInteractor;
 import cmput301f18t18.health_detective.domain.model.BodyLocation;
+import cmput301f18t18.health_detective.domain.model.images.BodyImage;
 
 class AddBodyLocationImpl extends AbstractInteractor implements AddBodyLocation {
 
     private AddBodyLocation.Callback callback;
     private String label;
-    private BodyLocation bodyLocation;
+    private BodyImage bodyImage;
     private Integer xPos;
     private Integer yPos;
 
     public AddBodyLocationImpl(AddBodyLocation.Callback callback,
-                               String label, BodyLocation bodyLocation, Integer xPos, Integer yPos) {
+                               String label, BodyImage bodyImage, Integer xPos, Integer yPos) {
         this.callback = callback;
         this.label = label;
-        this.bodyLocation = bodyLocation;
+        this.bodyImage = bodyImage;
         this.xPos = xPos;
         this.yPos = yPos;
     }
