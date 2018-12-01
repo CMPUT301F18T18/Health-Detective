53acfcb7fda767e0ab59278886416c13089db11b
walkP <wpeters@ualberta.ca>
2018-12-01 13:36:50 -0700

    FINALLY got some intent testing working, more to come

--- app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivityTest.java
+++ app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivityTest.java
@@ -1,166 +1,114 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
+
+import android.content.ComponentName;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cmput301f18t18.health_detective.R;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.Espresso.pressBack;
+
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SignUpActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule<SignUpActivity> SUActivityTestRule =
+            new IntentsTestRule<>(SignUpActivity.class);
 
+    //Making sure invalid data invokes correct toast and does not change activities
     @Test
-    public void signUpActivityTest() {
+    public void invalidDataTest() {
+        //Checking invalid ID
+        onView(withId(R.id.userEdit))
+                .perform(replaceText("walker"),closeSoftKeyboard());
+        onView(withId(R.id.emailEdit))
+                .perform(replaceText("validemail@email.com"),closeSoftKeyboard());
+        onView(withId(R.id.phoneNumEdit))
+                .perform(replaceText("(780) 222-2222"),closeSoftKeyboard());
+        onView(withId(R.id.signUpBtn)).perform(click());
+        onView(withText("Invalid Id"))
+                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
 
-        String userID = "walker2029";
-        ViewInteraction appCompatTextView = onView(
-                allOf(withId(R.id.signUpText), withText("Sign up"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatTextView.perform(click());
+        //Checking invalid Email
+        onView(withId(R.id.userEdit))
+                .perform(replaceText("spongebob99"),closeSoftKeyboard());
+        onView(withId(R.id.emailEdit))
+                .perform(replaceText("invalidemail"),closeSoftKeyboard());
+        onView(withId(R.id.phoneNumEdit))
+                .perform(replaceText("(780) 222-2222"),closeSoftKeyboard());
+        onView(withId(R.id.signUpBtn)).perform(click());
+        onView(withText("Invalid Email"))
+                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.userEdit),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText(userID), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.emailEdit),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("testemail@email.com"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.phoneNumEdit),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatEditText3.perform(replaceText("(780) 222-1111"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.signUpBtn), withText("Sign up"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction actionMenuItemView = onView(
-                allOf(withText(userID),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.action_bar),
-                                        1),
-                                0),
-                        isDisplayed()));
-        actionMenuItemView.perform(click());
-
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.userEdit), withText(userID),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        editText.check(matches(withText(userID)));
-
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.emailEdit), withText("testemail@email.com"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                4),
-                        isDisplayed()));
-        editText2.check(matches(withText("testemail@email.com")));
-
-        ViewInteraction editText3 = onView(
-                allOf(withId(R.id.phoneNumEdit), withText("(780) 222-1111"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                6),
-                        isDisplayed()));
-        editText3.check(matches(withText("(780) 222-1111")));
-
-        ViewInteraction checkBox = onView(
-                allOf(withId(R.id.PcheckBox),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                8),
-                        isDisplayed()));
-        checkBox.check(matches(isDisplayed()));
+        //Checking invalid phone number
+        onView(withId(R.id.userEdit))
+                .perform(replaceText("spongebob99"),closeSoftKeyboard());
+        onView(withId(R.id.emailEdit))
+                .perform(replaceText("validemail@email.com"),closeSoftKeyboard());
+        onView(withId(R.id.phoneNumEdit))
+                .perform(replaceText("7802222222"),closeSoftKeyboard());
+        onView(withId(R.id.signUpBtn)).perform(click());
+        onView(withText("Invalid Phone Number"))
+                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
 
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    //Making sure cancel button is working properly
+    @Test
+    public void SUCancelTest() {
+        onView(withId(R.id.cancelBtn)).perform(click());
+        assertTrue(SUActivityTestRule.getActivity().isFinishing());
+    }
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
+    //Making sure the sign up button stores info and sends you to correct activity
+    @Test
+    public void SUAcceptTest() {
 
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        //Generating random string for userID (for uniqueness)
+        int n = 10000000 + (int)(Math.random() * 90000000);
+        String userID = Integer.toString(n);
+        System.out.println(userID);
+        onView(withId(R.id.userEdit))
+                .perform(replaceText(userID),closeSoftKeyboard());
+        onView(withId(R.id.emailEdit))
+                .perform(replaceText("validemail@email.com"),closeSoftKeyboard());
+        onView(withId(R.id.phoneNumEdit))
+                .perform(replaceText("(780) 222-2222"),closeSoftKeyboard());
+        onView(withId(R.id.signUpBtn)).perform(click());
+        /*
+        Have to use withText instead of withID because view is in menu bar.
+        This also ensure we are in right activity because this would not be
+        in the view if we were not in the correct activity
+        */
+        onView(withText(userID)).check(matches(withText(userID)));
     }
 }
