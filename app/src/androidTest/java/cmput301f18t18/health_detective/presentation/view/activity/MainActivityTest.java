e4b889d572349e3aa376c74f93637c616e8b4587
walkP <wpeters@ualberta.ca>
2018-12-01 17:18:30 -0700

    Think I finished MainActivityTest, waiting for ElasticSearch to come back up

---
+++ app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/MainActivityTest.java
@@ -0,0 +1,63 @@
+package cmput301f18t18.health_detective.presentation.view.activity;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cmput301f18t18.health_detective.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertTrue;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mainActivityTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+    //Making sure invalid Id invokes correct toast and does not change activities
+    @Test
+    public void MainInvalidDataTest() {
+        //Checking invalid ID
+        onView(withId(R.id.userIdLogin))
+                .perform(replaceText("walker"),closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        onView(withText("Invalid UserId"))
+                .inRoot(withDecorView(not(mainActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+
+    }
+
+    //Making sure Sign up button is working properly and launching correct activity
+    @Test
+    public void MainSignUpButtonTest() {
+        onView(withId(R.id.signUpText)).perform(click());
+        intended(hasComponent(SignUpActivity.class.getName()));
+    }
+
+    //Making sure the login button logs in correct user and launches correct activity
+    @Test
+    public void MainAcceptTest() {
+        onView(withId(R.id.userIdLogin))
+                .perform(replaceText("walker2018"),closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        intended(hasComponent(PatientProblemsActivity.class.getName()));
+    }
+}
--- app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivityTest.java
+++ app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/SignUpActivityTest.java
@@ -1,12 +1,7 @@
 package cmput301f18t18.health_detective.presentation.view.activity;
 
-
-
-import android.content.ComponentName;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,22 +9,17 @@ import org.junit.runner.RunWith;
 
 import cmput301f18t18.health_detective.R;
 
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 
@@ -38,12 +28,12 @@ import static org.junit.Assert.assertTrue;
 public class SignUpActivityTest {
 
     @Rule
-    public IntentsTestRule<SignUpActivity> SUActivityTestRule =
+    public IntentsTestRule<SignUpActivity> suActivityTestRule =
             new IntentsTestRule<>(SignUpActivity.class);
 
     //Making sure invalid data invokes correct toast and does not change activities
     @Test
-    public void invalidDataTest() {
+    public void SUInvalidDataTest() {
         //Checking invalid ID
         onView(withId(R.id.userEdit))
                 .perform(replaceText("walker"),closeSoftKeyboard());
@@ -53,7 +43,7 @@ public class SignUpActivityTest {
                 .perform(replaceText("(780) 222-2222"),closeSoftKeyboard());
         onView(withId(R.id.signUpBtn)).perform(click());
         onView(withText("Invalid Id"))
-                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .inRoot(withDecorView(not(suActivityTestRule.getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
 
         //Checking invalid Email
@@ -65,7 +55,7 @@ public class SignUpActivityTest {
                 .perform(replaceText("(780) 222-2222"),closeSoftKeyboard());
         onView(withId(R.id.signUpBtn)).perform(click());
         onView(withText("Invalid Email"))
-                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .inRoot(withDecorView(not(suActivityTestRule .getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
 
         //Checking invalid phone number
@@ -77,7 +67,7 @@ public class SignUpActivityTest {
                 .perform(replaceText("7802222222"),closeSoftKeyboard());
         onView(withId(R.id.signUpBtn)).perform(click());
         onView(withText("Invalid Phone Number"))
-                .inRoot(withDecorView(not(SUActivityTestRule.getActivity().getWindow().getDecorView())))
+                .inRoot(withDecorView(not(suActivityTestRule.getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
 
     }
@@ -86,17 +76,15 @@ public class SignUpActivityTest {
     @Test
     public void SUCancelTest() {
         onView(withId(R.id.cancelBtn)).perform(click());
-        assertTrue(SUActivityTestRule.getActivity().isFinishing());
+        assertTrue(suActivityTestRule.getActivity().isFinishing());
     }
 
     //Making sure the sign up button stores info and sends you to correct activity
     @Test
     public void SUAcceptTest() {
-
         //Generating random string for userID (for uniqueness)
         int n = 10000000 + (int)(Math.random() * 90000000);
         String userID = Integer.toString(n);
-        System.out.println(userID);
         onView(withId(R.id.userEdit))
                 .perform(replaceText(userID),closeSoftKeyboard());
         onView(withId(R.id.emailEdit))
