f6f87ae63e4407253cc9c1b4a2a4d4228294eb2f
walkP <wpeters@ualberta.ca>
2018-12-02 22:24:55 -0700

    Started on MapActivity intent testing - will finish in morning along with others

--- app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/MapActivityTest.java
+++ app/src/androidTest/java/cmput301f18t18/health_detective/presentation/view/activity/MapActivityTest.java
@@ -7,15 +7,28 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Date;
 
 import cmput301f18t18.health_detective.R;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
 
 
 @LargeTest
@@ -32,6 +45,53 @@ public class MapActivityTest {
         onView(withId(R.id.userIdLogin))
                 .perform(replaceText("walker2018"), closeSoftKeyboard());
         onView(withId(R.id.loginButton)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("OKAY")).perform(click());
+        onView(withText("OKAY")).perform(click());
+        onView(withId(R.id.addProbBtn)).perform(click());
+        onView(withId(R.id.problemTitle))
+                .perform(replaceText("RecordTest problem"));
+        Date date = new Date();
+        onView(withId(R.id.problemDate)).perform(replaceText(date.toString()));
+        onView(withId(R.id.problemDesc))
+                .perform(replaceText("RecordTest Description"));
+        onView(withId(R.id.saveBtn)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onData(anything()).inAdapterView((withId(R.id.problemListView)))
+                .atPosition(0)
+                .onChildView(withId(R.id.recordsBut))
+                .perform(click());
+        onView(withText("OKAY")).perform(click());
+        onView(withText("OKAY")).perform(click());
+        onView(withId(R.id.addRecordsBtn)).perform(click());
+        onView(withHint("Title")).perform(replaceText("TestRecord"));
+        onView(withText("SAVE")).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Making sure cancel button is working properly
+    @Test
+    public void MAPCancelButtonTest() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.editmapbut)).perform(click());
+        onView(withText("CANCEL")).perform(click());
+        intended(hasComponent(PatientRecordViewActivity.class.getName()));
     }
 
 }
